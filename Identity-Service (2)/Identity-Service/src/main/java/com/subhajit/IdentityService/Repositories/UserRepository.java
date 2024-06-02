1c3e13d00b65d1b743ea73894aa5841235737b7a
subhajitpatra498 <subhajitpatra498@gmail.com>
2024-06-02 21:14:00 +0530

    update ✔✔

--- Identity-Service (2)/Identity-Service/src/main/java/com/subhajit/IdentityService/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.subhajit.IdentityService.Repositories;
-
-import com.subhajit.IdentityService.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-public interface UserRepository extends JpaRepository<User,Integer> {
-    Optional<User> findByEmail(String email);
-    Boolean existsByEmail(String email);
-}
